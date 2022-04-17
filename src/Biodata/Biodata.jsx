79d9045b66539e8c751704f27bfacc14e9fbe5ab
designeralabi <bdlkrmalabi@gmail.com>
2022-04-17 18:17:06 +0100

    added imageProfiler components

---
+++ src/Biodata/Biodata.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const biodata = () => {
+  return (
+    <div>
+          <h1>Welcome to my portfolio cv</h1>
+    </div>
+  );
+};
+
+export default biodata;
---
+++ src/Biodata/Imageprofiler.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Imageprofiler = () => {
+  return <img src="" alt="alabi-image"></img>;
+};
+
+export default Imageprofiler;

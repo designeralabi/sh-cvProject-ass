d30a06ead377744a9922105cf04898403ee59422
designeralabi <bdlkrmalabi@gmail.com>
2022-04-18 18:32:03 +0100

    added few components

--- src/Biodata/Biodata.jsx
+++ src/Biodata/Biodata.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import SocialIcons from "../SocialIcons/SocialIcons";
 const biodata = () => {
   return (
     <div>
-          <h1>Welcome to my portfolio cv</h1>
+      <h1>Welcome to my portfolio cv</h1>
+      <SocialIcons />
     </div>
   );
 };
---
+++ src/SocialIcons/SocialIcons.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+const socialIcons = () => {
+  return (
+    <div>
+      <li>
+        <a href="">
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <i class="fa fa-github" aria-hidden="true"></i>
+        </a>
+      </li>
+    </div>
+  );
+};
+
+export default socialIcons;

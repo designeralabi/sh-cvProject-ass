7b47485ab53d3908105f459f120ae1238e75232e
designeralabi <bdlkrmalabi@gmail.com>
2022-04-19 05:09:13 +0100

    fixed social-links

--- src/SocialIcons/SocialIcons.jsx
+++ src/SocialIcons/SocialIcons.jsx
@@ -2,21 +2,23 @@ import React from "react";
 const socialIcons = () => {
   return (
     <div>
-      <li>
-        <a href="">
-          <i class="fa fa-instagram" aria-hidden="true"></i>
-        </a>
-      </li>
-      <li>
-        <a href="">
-          <i class="fa fa-twitter" aria-hidden="true"></i>
-        </a>
-      </li>
-      <li>
-        <a href="">
-          <i class="fa fa-github" aria-hidden="true"></i>
-        </a>
-      </li>
+      <ul>
+        <li>
+          <a href="https://twitter.com/code_kareem">
+            <i class="fa fa-instagram" aria-hidden="true">instagram</i>
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/code_kareem">
+            <i class="fa fa-twitter" aria-hidden="true">twitter</i>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/designeralabi">
+            <i class="fa fa-github" aria-hidden="true">github</i>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };

957f4c0a1ce9cb91b0aaaed8c8a58570faf23154
designeralabi <bdlkrmalabi@gmail.com>
2022-04-19 04:55:06 +0100

    add images folder

--- src/Biodata/Imageprofiler.jsx
+++ src/Biodata/Imageprofiler.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import ProfilePics from "../Images/new dp.jpg";
 
 const Imageprofiler = () => {
-  return <img src="" alt="alabi-image"></img>;
+  return <img className="profile-pics" src={ProfilePics} alt="alabi"></img>;
 };
 
 export default Imageprofiler;
